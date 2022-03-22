90ba5877d00b270a946997d41a4eb60bed3491ae
lana <lana.translations@gmail.com>
2022-03-22 03:10:39 -0700

    added names of pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert ("Hello, World!");
-let favoriteFood='Watermelon';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: "Pikachu", type: "Fire", height ="2'3"},
+  {name: "Eevee", type: "Air", height ="2'5"},
+  {name: "Charizard", type: "Fire", height ="7'4"},
+  {name: "Snorlax", type: "Water", height ="6'1"}
+];git
