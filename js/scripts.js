bd17d1799d7d1c7d81970eb3b6efb7928128f266
lana <lana.translations@gmail.com>
2022-03-22 19:45:19 -0700

    change equal signs to semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-  {name: "Pikachu", type: "Fire", height ="2'3"},
-  {name: "Eevee", type: "Air", height ="2'5"},
-  {name: "Charizard", type: ["Fire", "Air"], height ="7'4"},
-  {name: "Snorlax", type: ["Water", "Ice"], height ="6'1"}
+  {name: "Pikachu", type: "Fire", height: "2'3"},
+  {name: "Eevee", type: "Air", height: "2'5"},
+  {name: "Charizard", type: ["Fire", "Air"], height:"7'4"},
+  {name: "Snorlax", type: ["Water", "Ice"], height: "6'1"}
 ];
