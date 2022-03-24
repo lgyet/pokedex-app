3e10a5cadc6bb4a860a47e02c4ef6e56b0eeea57
lana <lana.translations@gmail.com>
2022-03-23 20:27:21 -0700

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,23 @@ let pokemonList = [
   {name: "Charizard", type: ["Fire", "Air"], height:"7'4"},
   {name: "Snorlax", type: ["Water", "Ice"], height: "6'1"}
 ];
+
+// let age = 22;
+// console.log("Your are "+ age +" years old.")  ;
+// if (age > 12 && age<20) {
+//   console.log('You are a teenager!')
+// } else if (age<13) {
+//   console.log('You are a child!')
+// } else if (age>20) {
+//   console.log('You are an adult.')
+// } else if (age>99) {
+//   console.log('You are about to become a child again!')
+// }
+
+for (let i=0; i < pokemonList.length; i++) {
+//  document.write(pokemonList.name(i) + " (height:) " + pokemonList.height(i));
+//};
+  document.write(pokemonList.name);
+  };
+
+console.log(pokemonList.name(1));
